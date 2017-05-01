806a49d1bdde736aabd6e103b31746cb65ef0698
prem <prem.bharti@yatra.com>
2017-05-01 12:32:42 +0530

    cleaning

---
+++ src/main/java/com/yatra/core/kafka/callbacks/ProducerCallback.java
@@ -0,0 +1,7 @@
+package com.yatra.core.kafka.callbacks;
+
+/**
+ * Created by prembharti on 1/5/17.
+ */
+public class ProducerCallback {
+}
---
+++ src/main/java/com/yatra/core/kafka/producers/AsyncProducer.java
@@ -0,0 +1,7 @@
+package com.yatra.core.kafka.producers;
+
+/**
+ * Created by prembharti on 1/5/17.
+ */
+public class AsyncProducer {
+}
